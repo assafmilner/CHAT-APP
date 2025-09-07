c059494891362d2aa12e7a78a434eccb6caca0dd
Assaf Milner <assafmilner@gmail.com>
2025-09-07 12:04:53 +0300

    Auth routes and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,21 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server is running on PORT:"+ PORT)
-  connectDB
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
 });
